a9a50791ea1a164b6fa2776fb2aa322eb968dbb3
Vishal seehra <seehravishal@gmail.com>
2026-03-23 23:06:49 +0530

    Add Teraform files

---
+++ terraform/ec2.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "app_server" {
+  ami           = "ami-0c02fb55956c7d316"  
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "restaurant-ec2"
+  }
+}
---
+++ terraform/security.tf
@@ -0,0 +1,17 @@
+resource "aws_security_group" "app_sg" {
+  name = "restaurant-sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
